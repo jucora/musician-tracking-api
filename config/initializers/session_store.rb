e1ae0830865be832a2988cc659ae4132a9d6071c
jucora <artjulius@hotmail.com>
2020-08-29 00:53:21 -0500

    Try to fix same_site cookie issue with secure: true option

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, domain: "https://musician-tracking-app.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, secure: true, domain: "https://musician-tracking-app.herokuapp.com/"
 else
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, secure: true
 end
