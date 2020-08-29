64843a59354cd3a574d7cc7802fb57f1f2545f5e
jucora <artjulius@hotmail.com>
2020-08-29 02:59:55 -0500

    Add https://musician-tracking-app.web.app/ in origin to fix cors policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     before_filter :cors_preflight_check
 
     def cors_preflight_check
-        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Origin'] = 'https://musician-tracking-app.web.app/'
         headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
         headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
