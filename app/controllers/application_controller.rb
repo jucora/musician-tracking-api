2235463ba2d1aa5919dfc7d963e33a3c00b9c4dd
jucora <artjulius@hotmail.com>
2020-08-29 16:18:24 -0500

    Remove before filter from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-
-    before_filter :cors_preflight_check
-
-    def cors_preflight_check
-        headers['Access-Control-Allow-Origin'] = 'https://musician-tracking-app.web.app/'
-        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-        headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    end
 end
