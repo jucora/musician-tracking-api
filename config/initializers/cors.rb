5d69d76b77ad48919d5e52c97614a592d2590919
jucora <artjulius@hotmail.com>
2020-08-29 16:22:08 -0500

    Add production url in the first place in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
+  allow do
+    origins "https://musician-tracking-app.web.app/"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
     allow do
       origins "http://localhost:3000"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
-  
-    allow do
-      origins "https://musician-tracking-app.web.app/"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+    end  
 end
