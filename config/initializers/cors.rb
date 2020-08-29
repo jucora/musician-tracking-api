62301208affb18918b159b81b36c047b803c95e2
jucora <artjulius@hotmail.com>
2020-08-28 21:47:22 -0500

    Add url of the React app before deploy the API

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
   
     allow do
-      origins "https://your-production-app-here.com"
+      origins "https://musician-tracking-app.web.app/"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end
