fe9e482fc6556f25eb8b5237bb7e3eb979b69945
jucora <artjulius@hotmail.com>
2020-08-29 02:55:50 -0500

    Add skip_before_action to fix cors policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
+
+    before_filter :cors_preflight_check
+
+    def cors_preflight_check
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
 end
