32aada10db1ea72d37610236ad11b63e76418a24
jucora <artjulius@hotmail.com>
2020-09-02 09:59:53 -0500

    Add cookie _session_id in session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, secure: true, domain: "https://musician-tracking-app.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :strict, secure: true, domain: "https://musician-tracking-app.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_session_id", same_site: :lax, secure: true, domain: "https://musician-tracking-app.herokuapp.com/"
 else
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, secure: true
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :strict, secure: true
+    Rails.application.config.session_store :cookie_store, key: "_session_id", same_site: :lax, secure: true
 end
