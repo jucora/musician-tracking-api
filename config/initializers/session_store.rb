29e99c806e7b0d650915698a86aa90f252bf13df
jucora <artjulius@hotmail.com>
2020-09-02 15:59:17 -0500

    Add origins including proxy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :strict, secure: true, domain: "https://musician-tracking-app.herokuapp.com/"
-    Rails.application.config.session_store :cookie_store, key: "_session_id", same_site: :lax, secure: true, domain: "https://musician-tracking-app.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :strict, secure: true, domain: "https://cors-anywhere.herokuapp.com/https://musician-tracking-app.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_session_id", same_site: :lax, secure: true, domain: "https://cors-anywhere.herokuapp.com/https://musician-tracking-app.herokuapp.com/"
 else
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :strict, secure: true
-    Rails.application.config.session_store :cookie_store, key: "_session_id", same_site: :lax, secure: true
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :strict
 end
