0d05652089b0d4faeaea67ad4466d3dad62932a0
jucora <artjulius@hotmail.com>
2020-09-03 00:57:13 -0500

    Add app urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
-    origins "https://cors-anywhere.herokuapp.com/https://musician-tracking-app.web.app/"
+    origins "https://musician-tracking-app.web.app"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
