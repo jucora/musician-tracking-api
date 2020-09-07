c865cacefe32abbbc335db90e216a1aabc72ef21
jucora <artjulius@hotmail.com>
2020-09-07 17:39:23 -0500

    Add private method session_params in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
     include CurrentUserConcern
 
-    def create
+    def create 
         user = User
-                .find_by(email: params["user"]["email"])
-                .try(:authenticate, params["user"]["password"])
+                .find_by(email: session_params[:email])
+                .try(:authenticate, session_params[:password])
     
         if user
           session[:user_id] = user.id
@@ -37,5 +37,9 @@ class SessionsController < ApplicationController
             status: 200,
             logged_out: true
         }
+    end
+    private
+    def session_params
+        params.require(:user).permit(:email, :password)
     end
 end
